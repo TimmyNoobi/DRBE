9b8ac424b96cffff294c07fc0feb20f386c2370d
timling <timling@g.ucla.edu>
2020-02-10 20:22:31 -0800

    Add project files.

---
+++ DRBE/MainPage.xaml.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Collections;
+using Windows.Devices.Power;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
+using Windows.UI;
+using Windows.UI.Popups;
+using Windows.Devices.SerialCommunication;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+using System.Collections.ObjectModel;
+using Windows.Storage;
+using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+using System.Collections.Concurrent;
+using Windows.UI.Xaml.Documents;
+using Windows.Storage.Search;
+using Windows.Devices.Usb;
+using System.Text;
+using Windows.UI.Text;
+using Windows.ApplicationModel;
+
+using System.Net;
+using System.Net.Sockets;
+
+using System.Net.Http;
+using Windows.Data.Pdf;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DRBE
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        #region color & brush
+        private Color PLS_Config_canvas_color = Color.FromArgb((byte)255, (byte)64, (byte)80, (byte)80);
+        private Color Back_color = Color.FromArgb((byte)255, (byte)32, (byte)32, (byte)32);
+        private SolidColorBrush Transparent_brush = new SolidColorBrush(Color.FromArgb((byte)0, (byte)0, (byte)0, (byte)0));
+        private SolidColorBrush Dodge_blue_brush = new SolidColorBrush(Color.FromArgb((byte)255, (byte)30, (byte)144, (byte)255));
+        private SolidColorBrush Default_back_black_color_brush = new SolidColorBrush(Color.FromArgb((byte)255, (byte)32, (byte)32, (byte)32));
+        private SolidColorBrush Light_back_black_color_brush = new SolidColorBrush(Color.FromArgb((byte)255, (byte)42, (byte)42, (byte)42));
+        private SolidColorBrush white_button_brush = new SolidColorBrush(Color.FromArgb((byte)255, (byte)250, (byte)250, (byte)250));
+        private SolidColorBrush green_text_brush = new SolidColorBrush(Color.FromArgb((byte)255, (byte)140, (byte)255, (byte)140));
+        private SolidColorBrush green_button_brush = new SolidColorBrush(Color.FromArgb((byte)40, (byte)20, (byte)230, (byte)20));
+        private SolidColorBrush Teal_color = new SolidColorBrush(Color.FromArgb((byte)255, (byte)84, (byte)100, (byte)130));
+        private SolidColorBrush Blue_color = new SolidColorBrush(Color.FromArgb((byte)255, (byte)34, (byte)50, (byte)70));
+        private SolidColorBrush Sky_blue_color = new SolidColorBrush(Color.FromArgb((byte)255, (byte)170, (byte)170, (byte)255));
+        private SolidColorBrush Sky_green_color = new SolidColorBrush(Color.FromArgb((byte)255, (byte)170, (byte)255, (byte)170));
+        private SolidColorBrush Sky_red_color = new SolidColorBrush(Color.FromArgb((byte)255, (byte)255, (byte)170, (byte)170));
+        private SolidColorBrush Blue_Violet = new SolidColorBrush(Color.FromArgb((byte)255, (byte)138, (byte)43, (byte)226));
+        private SolidColorBrush Deep_Pink = new SolidColorBrush(Color.FromArgb((byte)255, (byte)255, (byte)20, (byte)147));
+        private SolidColorBrush Sea_Green = new SolidColorBrush(Color.FromArgb((byte)255, (byte)46, (byte)139, (byte)87));
+        private SolidColorBrush Violet_Red = new SolidColorBrush(Color.FromArgb((byte)255, (byte)199, (byte)21, (byte)112));
+        private SolidColorBrush Navy = new SolidColorBrush(Color.FromArgb((byte)255, (byte)0, (byte)0, (byte)128));
+        private SolidColorBrush Light_blue_color = new SolidColorBrush(Color.FromArgb((byte)255, (byte)210, (byte)230, (byte)250));
+        private SolidColorBrush red_button_brush = new SolidColorBrush(Color.FromArgb((byte)40, (byte)230, (byte)20, (byte)20));
+        private SolidColorBrush yellow_brush = new SolidColorBrush(Color.FromArgb((byte)200, (byte)250, (byte)250, (byte)20));
+        private SolidColorBrush Light_blue_brush = new SolidColorBrush(Color.FromArgb((byte)250, (byte)173, (byte)216, (byte)230));
+        private SolidColorBrush orange_brush = new SolidColorBrush(Color.FromArgb((byte)255, (byte)255, (byte)180, (byte)50));
+        private SolidColorBrush ardu_brush = new SolidColorBrush(Color.FromArgb((byte)250, (byte)0, (byte)139, (byte)139));
+        private SolidColorBrush dark_grey_brush = new SolidColorBrush(Color.FromArgb((byte)250, (byte)169, (byte)169, (byte)169));
+        private SolidColorBrush red_bright_button_brush = new SolidColorBrush(Color.FromArgb((byte)255, (byte)230, (byte)10, (byte)10));
+        private SolidColorBrush green_bright_button_brush = new SolidColorBrush(Color.FromArgb((byte)255, (byte)10, (byte)250, (byte)10));
+        private SolidColorBrush green_ready_brush = new SolidColorBrush(Color.FromArgb((byte)155, (byte)20, (byte)230, (byte)20));
+        private Brush textwhite = new SolidColorBrush(Color.FromArgb((byte)255, (byte)250, (byte)250, (byte)250));
+        private List<SolidColorBrush> All_Color = new List<SolidColorBrush>();
+        #endregion
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        #region communication
+        static string UWportnumber = "8181";
+        private Windows.Networking.Sockets.StreamSocket UWstreamsocket = new Windows.Networking.Sockets.StreamSocket();
+        private Windows.Networking.HostName UWhostname = new Windows.Networking.HostName("localhost");
+        private Stream UWinputstream;
+        private Stream UWoutputstream;
+        private StreamReader UWstreamreader;
+        private StreamWriter UWstreamwriter;
+        private BinaryReader UWbinaryreader;
+        private BinaryWriter UWbinarywriter;
+        private bool UWconnectedflag = false;
+        private bool UWreaderflag = false;
+        private bool UWwriterflag = false;
+
+        private DRBE_TCP_Listener DTL = new DRBE_TCP_Listener();
+
+        private async void StartClient()
+        {
+            try
+            {
+                await UWstreamsocket.ConnectAsync(UWhostname, UWportnumber);
+
+                UWconnectedflag = true;
+                UWoutputstream = UWstreamsocket.OutputStream.AsStreamForWrite();
+                UWinputstream = UWstreamsocket.InputStream.AsStreamForRead();
+                UWstreamwriter = new StreamWriter(UWoutputstream);
+                UWbinarywriter = new BinaryWriter(UWoutputstream);
+                UWwriterflag = true;
+                UWstreamreader = new StreamReader(UWinputstream);
+                UWbinaryreader = new BinaryReader(UWinputstream);
+                UWreaderflag = true;
+
+                await ShowDialog("connected", "Connected");
+
+                //break;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        // State object for reading client data asynchronously  
+
+
+
+        #endregion
+        #region others
+            private async Task ShowDialog(string x, string y)
+        {
+            ContentDialog noWifiDialog = new ContentDialog
+            {
+                Title = x,
+                Content = y,
+                CloseButtonText = "Ok"
+            };
+
+            ContentDialogResult result = await noWifiDialog.ShowAsync();
+        }
+        private double S_D(string x)
+        {
+            double result = 0;
+            double sign = 1;
+            string before = "";
+            string after = "";
+            int i = 0;
+            int tenpower = 1;
+            int len = x.Length;
+            int beforeflag = 0;
+            if (len >= 1)
+            {
+                if (x[0] == '-')
+                {
+                    sign = -1;
+                }
+            }
+            while (i < len)
+            {
+                if (beforeflag == 0 && x[i] != '.')
+                {
+                    before += x[i].ToString();
+                }
+                else if (beforeflag == 1 && x[i] != '.')
+                {
+                    after += x[i].ToString();
+                    tenpower = tenpower * 10;
+                }
+                else if (x[i] == '.')
+                {
+                    beforeflag = 1;
+                }
+                else if (x[i] == '-')
+                {
+                    //sign = -1;
+                }
+                else
+                {
+                    //sign = -1;
+                }
+                i++;
+            }
+            result = (double)S_I(before) + ((double)S_I(after)) / tenpower;
+            result = result * sign;
+            return result;
+        }
+        private int S_I(string x)
+        {
+            int result = 0;
+            int index = 0;
+            int rindex = 0;
+            index = x.Length;
+            while (index > 0)
+            {
+                if (C_I(x[rindex]) != -1)
+                {
+                    result = result * 10 + C_I(x[rindex]);
+                }
+                else
+                {
+
+                }
+
+                rindex++;
+                index--;
+            }
+            return result;
+        }
+        private int C_I(char x)
+        {
+            int reint = 0;
+            if (x == '0')
+            {
+                reint = 0;
+            }
+            else if (x == '1')
+            {
+                reint = 1;
+            }
+            else if (x == '2')
+            {
+                reint = 2;
+
+            }
+            else if (x == '3')
+            {
+                reint = 3;
+            }
+            else if (x == '4')
+            {
+                reint = 4;
+            }
+            else if (x == '5')
+            {
+                reint = 5;
+            }
+            else if (x == '6')
+            {
+                reint = 6;
+            }
+            else if (x == '7')
+            {
+                reint = 7;
+            }
+            else if (x == '8')
+            {
+                reint = 8;
+            }
+            else if (x == '9')
+            {
+                reint = 9;
+            }
+            else
+            {
+                reint = -1;
+            }
+            return reint;
+        }
+        private int S_H(string x)
+        {
+            int result = 0;
+            int index = 0;
+            int rindex = 0;
+            index = x.Length;
+            while (index > 0)
+            {
+                if (C_H(x[rindex]) != -1)
+                {
+                    result = result * 16 + C_H(x[rindex]);
+                }
+                else
+                {
+
+                }
+
+                rindex++;
+                index--;
+            }
+            return result;
+        }
+        private int C_H(char x)
+        {
+            int reint = 0;
+            if (x == '0')
+            {
+                reint = 0;
+            }
+            else if (x == '1')
+            {
+                reint = 1;
+            }
+            else if (x == '2')
+            {
+                reint = 2;
+
+            }
+            else if (x == '3')
+            {
+                reint = 3;
+            }
+            else if (x == '4')
+            {
+                reint = 4;
+            }
+            else if (x == '5')
+            {
+                reint = 5;
+            }
+            else if (x == '6')
+            {
+                reint = 6;
+            }
+            else if (x == '7')
+            {
+                reint = 7;
+            }
+            else if (x == '8')
+            {
+                reint = 8;
+            }
+            else if (x == '9')
+            {
+                reint = 9;
+            }
+            else if (x == 'a' || x == 'A')
+            {
+                reint = 10;
+            }
+            else if (x == 'b' || x == 'B')
+            {
+                reint = 11;
+            }
+            else if (x == 'c' || x == 'C')
+            {
+                reint = 12;
+            }
+            else if (x == 'd' || x == 'D')
+            {
+                reint = 13;
+            }
+            else if (x == 'e' || x == 'E')
+            {
+                reint = 14;
+            }
+            else if (x == 'f' || x == 'F')
+            {
+                reint = 15;
+            }
+            else
+            {
+                reint = -1;
+            }
+            return reint;
+        }
+        #endregion
+
+        private async void MainPage_loaded(object sender, RoutedEventArgs e)
+        {
+            DTL.StartListening();
+        }
+
+        private void MainPage_sizechange(object sender, SizeChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DRBE/TCP_Listener.cs
@@ -0,0 +1,431 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Collections;
+using Windows.Devices.Power;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
+using Windows.UI;
+using Windows.UI.Popups;
+using Windows.Devices.SerialCommunication;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+using System.Collections.ObjectModel;
+using Windows.Storage;
+using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+using System.Collections.Concurrent;
+using Windows.UI.Xaml.Documents;
+using Windows.Storage.Search;
+using Windows.Devices.Usb;
+using System.Text;
+using Windows.UI.Text;
+using Windows.ApplicationModel;
+
+using System.Net;
+using System.Net.Sockets;
+
+using System.Net.Http;
+using Windows.Data.Pdf;
+
+namespace DRBE
+{
+    public class DRBE_TCP_Listener
+    {
+
+        public class StateObject
+        {
+            // Client  socket.  
+            public Socket workSocket = null;
+            // Size of receive buffer.  
+            public const int BufferSize = 1024;
+            // Receive buffer.  
+            public byte[] buffer = new byte[BufferSize];
+            // Received data string.  
+            public StringBuilder sb = new StringBuilder();
+        }
+
+        // Thread signal.  
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        public DRBE_TCP_Listener()
+        {
+        }
+
+        public async void StartListening()
+        {
+            // Establish the local endpoint for the socket.  
+            // The DNS name of the computer  
+            // running the listener is "host.contoso.com".  
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            //IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 8181);
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 8181);
+            // Create a TCP/IP socket.  
+            Socket listener = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+
+            // Bind the socket to the local endpoint and listen for incoming connections.  
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+
+                while (true)
+                {
+                    // Set the event to nonsignaled state.  
+                    allDone.Reset();
+
+                    // Start an asynchronous socket to listen for connections.  
+                    await ShowDialog(localEndPoint.ToString(), ipAddress.ToString());
+                    listener.BeginAccept(
+                        new AsyncCallback(AcceptCallback),
+                        listener);
+                    await ShowDialog(ipAddress.AddressFamily.ToString(), "Done");
+                    // Wait until a connection is made before continuing.  
+                    allDone.WaitOne();
+                    await ShowDialog("Done", "ALL");
+                }
+
+            }
+            catch (Exception e)
+            {
+                await ShowDialog("Connecting", e.ToString());
+                //Console.WriteLine(e.ToString());
+            }
+
+            //Console.WriteLine("\nPress ENTER to continue...");
+            //Console.Read();
+
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            // Signal the main thread to continue.  
+            allDone.Set();
+
+            // Get the socket that handles the client request.  
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            // Create the state object.  
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                new AsyncCallback(ReadCallback), state);
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+
+            // Retrieve the state object and the handler socket  
+            // from the asynchronous state object.  
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            // Read data from the client socket.   
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                // There  might be more data, so store the data received so far.  
+                state.sb.Append(Encoding.ASCII.GetString(
+                    state.buffer, 0, bytesRead));
+
+                // Check for end-of-file tag. If it is not there, read   
+                // more data.  
+                content = state.sb.ToString();
+                if (content.IndexOf("<EOF>") > -1)
+                {
+                    // All the data has been read from the   
+                    // client. Display it on the console.  
+                    Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
+                        content.Length, content);
+                    // Echo the data back to the client.  
+                    Send(handler, content);
+                }
+                else
+                {
+                    // Not all data received. Get more.  
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReadCallback), state);
+                }
+            }
+        }
+
+        private static void Send(Socket handler, String data)
+        {
+            // Convert the string data to byte data using ASCII encoding.  
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            // Begin sending the data to the remote device.  
+            handler.BeginSend(byteData, 0, byteData.Length, 0,
+                new AsyncCallback(SendCallback), handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.  
+                Socket handler = (Socket)ar.AsyncState;
+
+                // Complete sending the data to the remote device.  
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        #region others
+        private async Task ShowDialog(string x, string y)
+        {
+            ContentDialog noWifiDialog = new ContentDialog
+            {
+                Title = x,
+                Content = y,
+                CloseButtonText = "Ok"
+            };
+
+            ContentDialogResult result = await noWifiDialog.ShowAsync();
+        }
+        private double S_D(string x)
+        {
+            double result = 0;
+            double sign = 1;
+            string before = "";
+            string after = "";
+            int i = 0;
+            int tenpower = 1;
+            int len = x.Length;
+            int beforeflag = 0;
+            if (len >= 1)
+            {
+                if (x[0] == '-')
+                {
+                    sign = -1;
+                }
+            }
+            while (i < len)
+            {
+                if (beforeflag == 0 && x[i] != '.')
+                {
+                    before += x[i].ToString();
+                }
+                else if (beforeflag == 1 && x[i] != '.')
+                {
+                    after += x[i].ToString();
+                    tenpower = tenpower * 10;
+                }
+                else if (x[i] == '.')
+                {
+                    beforeflag = 1;
+                }
+                else if (x[i] == '-')
+                {
+                    //sign = -1;
+                }
+                else
+                {
+                    //sign = -1;
+                }
+                i++;
+            }
+            result = (double)S_I(before) + ((double)S_I(after)) / tenpower;
+            result = result * sign;
+            return result;
+        }
+        private int S_I(string x)
+        {
+            int result = 0;
+            int index = 0;
+            int rindex = 0;
+            index = x.Length;
+            while (index > 0)
+            {
+                if (C_I(x[rindex]) != -1)
+                {
+                    result = result * 10 + C_I(x[rindex]);
+                }
+                else
+                {
+
+                }
+
+                rindex++;
+                index--;
+            }
+            return result;
+        }
+        private int C_I(char x)
+        {
+            int reint = 0;
+            if (x == '0')
+            {
+                reint = 0;
+            }
+            else if (x == '1')
+            {
+                reint = 1;
+            }
+            else if (x == '2')
+            {
+                reint = 2;
+
+            }
+            else if (x == '3')
+            {
+                reint = 3;
+            }
+            else if (x == '4')
+            {
+                reint = 4;
+            }
+            else if (x == '5')
+            {
+                reint = 5;
+            }
+            else if (x == '6')
+            {
+                reint = 6;
+            }
+            else if (x == '7')
+            {
+                reint = 7;
+            }
+            else if (x == '8')
+            {
+                reint = 8;
+            }
+            else if (x == '9')
+            {
+                reint = 9;
+            }
+            else
+            {
+                reint = -1;
+            }
+            return reint;
+        }
+        private int S_H(string x)
+        {
+            int result = 0;
+            int index = 0;
+            int rindex = 0;
+            index = x.Length;
+            while (index > 0)
+            {
+                if (C_H(x[rindex]) != -1)
+                {
+                    result = result * 16 + C_H(x[rindex]);
+                }
+                else
+                {
+
+                }
+
+                rindex++;
+                index--;
+            }
+            return result;
+        }
+        private int C_H(char x)
+        {
+            int reint = 0;
+            if (x == '0')
+            {
+                reint = 0;
+            }
+            else if (x == '1')
+            {
+                reint = 1;
+            }
+            else if (x == '2')
+            {
+                reint = 2;
+
+            }
+            else if (x == '3')
+            {
+                reint = 3;
+            }
+            else if (x == '4')
+            {
+                reint = 4;
+            }
+            else if (x == '5')
+            {
+                reint = 5;
+            }
+            else if (x == '6')
+            {
+                reint = 6;
+            }
+            else if (x == '7')
+            {
+                reint = 7;
+            }
+            else if (x == '8')
+            {
+                reint = 8;
+            }
+            else if (x == '9')
+            {
+                reint = 9;
+            }
+            else if (x == 'a' || x == 'A')
+            {
+                reint = 10;
+            }
+            else if (x == 'b' || x == 'B')
+            {
+                reint = 11;
+            }
+            else if (x == 'c' || x == 'C')
+            {
+                reint = 12;
+            }
+            else if (x == 'd' || x == 'D')
+            {
+                reint = 13;
+            }
+            else if (x == 'e' || x == 'E')
+            {
+                reint = 14;
+            }
+            else if (x == 'f' || x == 'F')
+            {
+                reint = 15;
+            }
+            else
+            {
+                reint = -1;
+            }
+            return reint;
+        }
+        #endregion
+    }
+}
